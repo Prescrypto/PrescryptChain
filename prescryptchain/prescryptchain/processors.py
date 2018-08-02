0d88a4c67ebe5f7d0a9eb072a3844460c044a7aa
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-08-02 14:40:12 -0500

    fix indent on processors file

--- prescryptchain/prescryptchain/processors.py
+++ prescryptchain/prescryptchain/processors.py
@@ -2,6 +2,7 @@
 # Define custom processors to all templates
 # This actions add custom var to templating use
 from django.conf import settings
- def add_wallet_url(request):
+
+def add_wallet_url(request):
     # add production var to templates
     return {"WALLET_URL": settings.WALLET_URL}
